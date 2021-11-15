17c440b13af33dab625b535c83ee583ede8ba11f
Oscar <o.millot@kiwilab.fr>
2021-11-15 16:19:11 +0100

    add procfile and gitignore

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const apiRoutes = require("./routes/routes")
 const cron = require("./tools/cron")
 let app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
