f83292efbf996d471fd8498b87a1d53240bf8241
Oscar <o.millot@kiwilab.fr>
2021-11-16 09:08:00 +0100

    changement procfile et pakage.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const apiRoutes = require("./routes/routes")
 const cron = require("./tools/cron")
 let app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
